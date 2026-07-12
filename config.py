80a9a87835e1c4c21db94a65270f1bc732cf5811
lubadpap-cmyk <lubadpap@gmail.com>
2026-07-12 16:54:00 +0300

    Add PREMIUM_DURATION_DAYS constant to config

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ CHECK_COOLDOWN = 1.5
 
 # Premium subscription cost placeholder / details
 PREMIUM_PRICE_STARS = 100
+PREMIUM_DURATION_DAYS = 30
 
 # Maximum number of username generation attempts per search loop
 MAX_USERNAME_SEARCH_TRIES = 400
